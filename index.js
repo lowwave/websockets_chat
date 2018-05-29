f5561a9bf6e079323db0165578381106a789bb92
Andrii Antosha <antosand@fel.cvut.cz>
2018-05-29 18:36:09 +0200

    complete functionality with broadcasting

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ app.use(express.static('public'));
 const io = socket(server);
 
 io.on('connection', (socket) => {
-    console.log('made socket connection');
-})
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', (data) => {
+        io.emit('chat', data);
+    });
+
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing');
+    })
+});
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,30 @@
 // Make connection
 const socket = io.connect(`http://localhost:4000`);
+
+// Query DOM
+const message = document.querySelector('#message');
+const btn = document.querySelector('#send');
+const output = document.querySelector('#output');
+const feedback = document.querySelector('#feedback');
+
+// Emit events
+btn.addEventListener('click', (event) => {
+    socket.emit('chat', {
+        message: message.value
+    });
+    message.value = '';
+});
+
+message.addEventListener('keypress', (event) => {
+    socket.emit('typing');
+});
+
+// Listen for event
+socket.on('chat', (data) => {
+    feedback.innerHTML = '';
+    output.innerHTML += `<p>${data.message}</p>`;
+});
+
+socket.on('typing', () => {
+    feedback.innerHTML = '<p><em>Someone is typing...</em></p>';
+});
